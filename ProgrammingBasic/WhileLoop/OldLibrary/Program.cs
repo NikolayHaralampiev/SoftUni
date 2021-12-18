2aed8921873cb6c47443671e22a48c658b9bfd75
NikolayHaralampiev <93036415+NikolayHaralampiev@users.noreply.github.com>
2021-12-18 18:32:30 +0200

    Programming Basic

---
+++ ProgrammingBasic/WhileLoop/OldLibrary/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Ако не се открие книгата - "The book you search is not here!" "You checked {брой} books."
+// Ако се отркрие - You checked {брой} books and found it."  
+
+string bookName = Console.ReadLine();
+int libraryCapacity = int.Parse(Console.ReadLine());
+int checkedBooksCount = 0;
+string input = string.Empty;
+
+
+while (checkedBooksCount < libraryCapacity && (input = Console.ReadLine()) != bookName)
+{
+    checkedBooksCount++;
+}
+
+
+Console.WriteLine(input == bookName ?
+    $"You checked {checkedBooksCount} books and found it." :
+    $"The book you search is not here!\nYou checked {checkedBooksCount} books.");
