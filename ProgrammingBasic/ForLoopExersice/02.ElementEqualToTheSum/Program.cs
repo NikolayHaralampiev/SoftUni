302a480aa7d2478613410557471ea15c0f854fac
NikolayHaralampiev <93036415+NikolayHaralampiev@users.noreply.github.com>
2021-12-18 18:11:55 +0200

    ProgrammingBasic

---
+++ ProgrammingBasic/ForLoopExersice/02.ElementEqualToTheSum/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var input = Convert.ToInt32(Console.ReadLine());
+var bigest = 0;
+var sum = 0;
+for (int i = 1; i <= input; i++)
+{
+    var number = Convert.ToInt32(Console.ReadLine());
+    sum = sum + number;
+    if (bigest < number)
+    {
+        bigest = number;
+    }
+}
+if (sum == bigest * 2)
+{
+    Console.WriteLine("Yes");
+    Console.WriteLine("Sum = " + bigest);
+}
+else
+{
+    Console.WriteLine("No");
+    Console.WriteLine("Diff = " + Math.Abs((sum - bigest) - bigest));
+}
---
+++ ProgrammingBasic/ForLoopExersice/03.EvenAndOdd/Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double n = double.Parse(Console.ReadLine());
+double oddSum = 0.0;
+double evenSum = 0.0;
+double oddMax = double.MinValue;
+double evenMax = double.MinValue;
+double oddMin = double.MaxValue;
+double evenMin = double.MaxValue;
+
+for (int i = 1; i <= n; i++)
+{
+    double num = double.Parse(Console.ReadLine());
+    if (i % 2 == 0)
+    {
+        if (num > evenMax)
+        {
+            evenMax = num;
+        }
+        if (num < evenMin)
+        {
+            evenMin = num;
+        }
+        evenSum += num;
+
+    }
+    else
+    {
+
+        if (num > oddMax)
+        {
+            oddMax = num;
+        }
+        if (num < oddMin)
+        {
+            oddMin = num;
+        }
+
+        oddSum += num;
+    }
+}
+Console.WriteLine("OddSum={0}", oddSum);
+
+
+if (oddMin == double.MaxValue)
+{
+
+    Console.WriteLine("OddMin=No");
+}
+else
+{
+    Console.WriteLine("OddMin={0}", oddMin);
+}
+if (oddMax == double.MinValue)
+{
+
+    Console.WriteLine("OddMax=No");
+}
+else
+{
+    Console.WriteLine("OddMax={0}", oddMax);
+}
+Console.WriteLine("EvenSum={0}", evenSum);
+if (evenMin == double.MaxValue)
+{
+
+    Console.WriteLine("EvenMin=No");
+}
+else
+{
+    Console.WriteLine("EvenMin={0}", evenMin);
+}
+if (evenMax == double.MinValue)
+{
+
+    Console.WriteLine("EvenMax=No");
+}
+else
+{
+    Console.WriteLine("EvenMax={0}", evenMax);
+}
