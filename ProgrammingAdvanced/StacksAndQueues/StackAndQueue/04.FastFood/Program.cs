4461ea5e3201f8e6a528ce81dd7145a7030ede98
NikolayHaralampiev <93036415+NikolayHaralampiev@users.noreply.github.com>
2022-01-22 19:21:44 +0200

    Stack And Queue

---
+++ ProgrammingAdvanced/StacksAndQueues/StackAndQueue/04.FastFood/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantitFood = int.Parse(Console.ReadLine());
+            List<int> orders = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Queue<int> line = new Queue<int>(orders);
+
+            Console.WriteLine(line.Max());
+
+            while (line.Count > 0)
+            {
+                if (quantitFood >= line.Peek())
+                {
+                    quantitFood -= line.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ",line)}");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Orders complete");
+        }
+    }
+}
---
+++ ProgrammingAdvanced/StacksAndQueues/StackAndQueue/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.BalancedParenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> parenthesStack = new Stack<char>();
+
+            foreach (var symbol in input)
+            {
+                if (parenthesStack.Any())
+                {
+                    char check = parenthesStack.Peek();
+                    if (check == '{' && symbol == '}')
+                    {
+                        parenthesStack.Pop();
+                        continue;
+                    }
+                    else if (check == '[' && symbol == ']')
+                    {
+                        parenthesStack.Pop();
+                        continue;
+                    }
+                    else if (check == '(' && symbol == ')')
+                    {
+                        parenthesStack.Pop();
+                        continue;
+                    }
+                }
+                parenthesStack.Push(symbol);
+            }
+
+            Console.WriteLine(!parenthesStack.Any() ? "YES":"NO");
+        }
+    }
+}
