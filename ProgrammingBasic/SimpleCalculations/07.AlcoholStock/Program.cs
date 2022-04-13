922a6536916ecc1058f5ad8c522930a320d573df
NikolayHaralampiev <93036415+NikolayHaralampiev@users.noreply.github.com>
2022-04-13 20:24:16 +0300

    Added next exercise

---
+++ ProgrammingBasic/SimpleCalculations/07.AlcoholStock/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _07.AlcoholStock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceWhiskey = double.Parse(Console.ReadLine());
+            double quantityBeer = double.Parse(Console.ReadLine());
+            double quantityWine = double.Parse(Console.ReadLine());
+            double quantityRakia = double.Parse(Console.ReadLine());
+            double quantityWhiskey = double.Parse(Console.ReadLine());
+
+            double rakiaPrice = priceWhiskey / 2;
+            double winePrice = rakiaPrice - (0.4 * rakiaPrice);
+            double beerPrice = rakiaPrice - (0.8 * rakiaPrice);
+
+            double sumRakia = quantityRakia * rakiaPrice;
+            double sumWine = quantityWine * winePrice;
+            double sumBeer = quantityBeer * beerPrice;
+            double sumWhiskey = quantityWhiskey * priceWhiskey;
+
+            Console.WriteLine($"{sumRakia + sumWine + sumBeer + sumWhiskey:F2}");
+
+        }
+    }
+}
