5f0a495db16b3cc0b1fe489fac17818ca2336a2c
NikolayHaralampiev <93036415+NikolayHaralampiev@users.noreply.github.com>
2022-04-13 20:10:58 +0300

    Added next exercise

---
+++ ProgrammingBasic/SimpleCalculations/06.CharityCampaign/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06.CharityCampaign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int numOfPastryCook = int.Parse(Console.ReadLine());
+            int numOfCakes = int.Parse(Console.ReadLine());
+            int numOfWaffles = int.Parse(Console.ReadLine());
+            int numOfPancakes = int.Parse(Console.ReadLine());
+
+            int sumOfCakes = numOfCakes * 45;
+            double sumOfWaffles = numOfWaffles * 5.80;
+            double sumOfPancakes = numOfPancakes * 3.20;
+
+            double sumOfDay = (sumOfCakes + sumOfWaffles + sumOfPancakes) * numOfPastryCook;
+            double sumOfCampaign = sumOfDay * days;
+            double sumAfterCosts = sumOfCampaign - (sumOfCampaign/8);
+
+            Console.WriteLine($"{sumAfterCosts:F2}");
+        }
+    }
+}
