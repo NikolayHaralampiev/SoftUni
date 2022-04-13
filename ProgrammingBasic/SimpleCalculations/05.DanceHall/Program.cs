b74e58985208273712df03cc57cd05c2557d3ddd
NikolayHaralampiev <93036415+NikolayHaralampiev@users.noreply.github.com>
2022-04-13 19:52:11 +0300

    Added next exercise

---
+++ ProgrammingBasic/SimpleCalculations/05.DanceHall/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05.DanceHall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double lenghtHall = double.Parse(Console.ReadLine());
+           double weightHall = double.Parse(Console.ReadLine());
+           double sideWardrobe = double.Parse(Console.ReadLine());
+
+            double sizeHall = (lenghtHall * 100) * (weightHall * 100);
+            double sizeWardrobe = (sideWardrobe * 100) * (sideWardrobe * 100);
+            double sizeBench = (sizeHall / 10);
+            double freeSpace = (sizeHall - sizeWardrobe - sizeBench);
+            double numOfDancers = (freeSpace / (40 + 7000));
+
+            Console.WriteLine(Math.Floor(numOfDancers));
+        }
+    }
+}
