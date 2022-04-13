ddbc8cc41939de6e434ffd108595159fd4a484a7
NikolayHaralampiev <93036415+NikolayHaralampiev@users.noreply.github.com>
2022-04-13 19:33:08 +0300

    Added sample exam

---
+++ ProgrammingBasic/SimpleCalculations/04.TailoringWorkshop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SewingWorkshop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfTables = int.Parse(Console.ReadLine());
+            double lenghtTable = double.Parse(Console.ReadLine());
+            double weightTable = double.Parse(Console.ReadLine());
+
+            double priceRectangular = numOfTables * (lenghtTable + 2 * 0.30) * (weightTable + 2 * 0.30);
+            double priceSquare = numOfTables * (lenghtTable / 2) * (lenghtTable / 2);
+
+            double dollarPrice = priceRectangular * 7 + priceSquare * 9;
+            double bgnPrice = dollarPrice * 1.85;
+
+            Console.WriteLine($"{dollarPrice:F2} USD");
+            Console.WriteLine($"{bgnPrice:F2} BGN");
+
+        }
+    }
+}
