7ab9760ccd214ecbfcb94dd0600fe6c6d57e29b3
NikolayHaralampiev <93036415+NikolayHaralampiev@users.noreply.github.com>
2022-05-05 21:02:31 +0300

    Added exercises

---
+++ ProgrammingFundamentals/DataTypeAndVariables - Exercise/09.SpiceMustFlow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _09.SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int totalAmount = 0;
+            int days = 0;
+
+            while (start >= 100)
+            {
+                totalAmount += start - 26;
+                days++;
+                start -= 10;
+            }
+
+            if (days > 0)
+            {
+                totalAmount -= 26;
+            }
+
+            Console.WriteLine(days);
+            Console.WriteLine(totalAmount);
+
+        }
+    }
+}
---
+++ ProgrammingFundamentals/DataTypeAndVariables - Exercise/10.Pokemon/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _10.Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int halfOfN = n / 2;
+
+            int targets = 0;
+
+            while (n >= m)
+            {
+                targets++;
+                n -= m;
+
+                if (halfOfN == n && y > 0)
+                {
+                    n /= y;
+                }
+
+            }
+
+            Console.WriteLine(n);
+            Console.WriteLine(targets);
+        }
+    }
+}
