7540965c888588316e0a8e158e1cfeb895db1545
NikolayHaralampiev <93036415+NikolayHaralampiev@users.noreply.github.com>
2022-05-05 19:18:00 +0300

    DataTypesAndVariables - Lab added

---
+++ ProgrammingFundamentals/DataTypesAndVariables - Lab/07.ConcatNames/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _07.ConcatNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            string result = firstName + delimiter + lastName;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ProgrammingFundamentals/DataTypesAndVariables - Lab/10.LowerToUpper/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _10.LowerToUpper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = char.Parse(Console.ReadLine());
+
+            if (symbol >= 65 && symbol <= 90)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else if (symbol >= 97 && symbol <= 122)
+            {
+                Console.WriteLine("lower-case");
+
+            }
+
+        }
+    }
+}
