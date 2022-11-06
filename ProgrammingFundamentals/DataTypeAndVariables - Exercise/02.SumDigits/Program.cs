10d355a6573da30ee272c174bebe9a974cc263c6
NikolayHaralampiev <93036415+NikolayHaralampiev@users.noreply.github.com>
2022-11-06 14:02:28 +0200

    Updated code

--- ProgrammingFundamentals/DataTypeAndVariables - Exercise/02.SumDigits/Program.cs
+++ ProgrammingFundamentals/DataTypeAndVariables - Exercise/02.SumDigits/Program.cs
@@ -6,17 +6,16 @@ namespace _02.SumDigits
     {
         static void Main(string[] args)
         {
-            int numToSum = int.Parse(Console.ReadLine());
-            int finalSum = 0;
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
 
-            while (numToSum !=0)
+            do
             {
-                int lastDigit = numToSum % 10;
-                finalSum += lastDigit;
-                numToSum /= 10;
+                 sum = sum + (number % 10);
+                 number = number / 10;
             }
-
-            Console.WriteLine(finalSum);
+            while (number > 0);
+            Console.WriteLine(sum);
         }
     }
 }
